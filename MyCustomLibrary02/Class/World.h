704a1091e7c549dfdffc80ce30cd62801e4299d9
your_name <lizelusdut@qq.com>
2017-11-22 17:15:09 +0800

    Project Init

---
+++ MyCustomLibrary02/Class/World.h
@@ -0,0 +1,13 @@
+//
+//  World.h
+//  MyCustomLibrary02
+//
+//  Created by lizelu on 2017/11/22.
+//  Copyright © 2017年 lizelu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface World : NSObject
+- (void)display;
+@end
